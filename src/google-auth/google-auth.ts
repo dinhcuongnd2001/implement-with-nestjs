a1ad3b54a0ad2da32b6a7eab305e2ca989331d8b
dncuong <dinhcuongnd2001@gmail.com>
2024-02-02 09:32:19 +0700

    feat: implement write log to docs with GoogleAuth module

--- src/google-auth/google-auth.ts
+++ src/google-auth/google-auth.ts
@@ -1,44 +1,37 @@
 // import { authenticate } from '@google-cloud/local-auth';
-import { OAuth2Client } from 'google-auth-library';
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import * as path from 'path';
 import { google } from 'googleapis';
 import { docs_v1 } from '@googleapis/docs';
 
 @Injectable()
-export class GoogleAuth implements OnModuleInit {
-  private auth;
+export class GoogleAuth {
   private Docs: docs_v1.Docs;
-  private Client;
 
-  async onModuleInit() {
-    // const auth = await authenticate({
-    //   keyfilePath: path.join(__dirname, '../../credentials.json'),
-    //   scopes: 'https://www.googleapis.com/auth/documents',
-    // });
-
+  constructor() {
     const auth = new google.auth.GoogleAuth({
       keyFile: path.join(__dirname, '../../credentials.json'),
       scopes: 'https://www.googleapis.com/auth/documents',
     });
     this.Docs = google.docs({ version: 'v1', auth });
-    this.Client = await auth.getClient();
-    this.auth = auth;
   }
-  async getDocument() {
-    // const docs = google.docs('v1');
-    // google.options({ auth: this.auth });
-    // const currentDocs = await docs.documents.get({
-    //   auth: this.auth,
-    //   documentId:
-    //     process.env.GG_DOCUMENT_ID ||
-    //     '1t_DyPR39WwFhE5pq7BLqHRmxDYuIrQMqx5bq1cNvgSg',
-    // });
-    // console.log('data :', currentDocs.data);
-    const docs = google.docs({ version: 'v1', auth: this.Client });
-    const data = await docs.documents.get({
-      auth: this.auth,
-      documentId: '1t_DyPR39WwFhE5pq7BLqHRmxDYuIrQMqx5bq1cNvgSg',
+
+  async writeLogToDocs(content: string) {
+    if (!process.env.GG_DOCUMENT_ID) return;
+    await this.Docs.documents.batchUpdate({
+      documentId: process.env.GG_DOCUMENT_ID,
+      requestBody: {
+        requests: [
+          {
+            insertText: {
+              location: {
+                index: 1,
+              },
+              text: `${content}\n`,
+            },
+          },
+        ],
+      },
     });
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { LoggerService } from './logger/logger.service';
 import { ValidationPipe } from '@nestjs/common';
-import { google } from 'googleapis';
-import { docs } from '@googleapis/docs';
-import * as path from 'path';
-import { authenticate } from '@google-cloud/local-auth';
-import { GoogleAuth } from './google-auth/google-auth';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -29,62 +24,10 @@ async function bootstrap() {
     }),
   );
 
-  GoogleAuth;
   // cath all exception http error
 
   // app.useGlobalFilters(new HttpExceptionFilter());
 
-  // initial:
-  const auth = new google.auth.GoogleAuth({
-    keyFile: path.join(__dirname, '../credentials.json'), // Path to your JSON key file
-    scopes: ['https://www.googleapis.com/auth/documents'], // Scope for Google Docs
-  });
-
-  async function writeGoogleDocs(documentId, requests) {
-    try {
-      const docs = google.docs({ version: 'v1', auth }); // Create a Google Docs API client
-
-      // Send a batchUpdate request to modify the document
-      const writer = await docs.documents.batchUpdate({
-        documentId, // ID of the document to update
-        requestBody: {
-          requests, // Array of requests detailing the changes to be made
-        },
-      });
-      return writer; // Return the response from the Google Docs API
-    } catch (error) {
-      console.error('error', error); // Log any errors that occur
-    }
-  }
-
-  async function readGoogleDocs(documentId: string) {
-    try {
-      const docs = google.docs({ version: 'v1', auth }); // Create a Google Docs API client
-
-      // Retrieve the document content
-      const response = await docs.documents.get({ documentId }); // ID of the document to read
-      return response.data; // Return the document data
-    } catch (error) {
-      console.error('error', error); // Log any errors that occur
-    }
-  }
-
-  // const data = await readGoogleDocs(
-  //   '1t_DyPR39WwFhE5pq7BLqHRmxDYuIrQMqx5bq1cNvgSg',
-  // );
-
-  await writeGoogleDocs('1t_DyPR39WwFhE5pq7BLqHRmxDYuIrQMqx5bq1cNvgSg', [
-    {
-      insertText: {
-        location: {
-          index: 1, // Specify the index to insert text
-        },
-        text: 'Hello CodingWithAdo Fans!\n', // Text to be inserted
-      },
-    },
-  ]);
-
-  // new GoogleAuth().getDocument();
   await app.listen(process.env.PORT || 3055);
 }
 bootstrap();
